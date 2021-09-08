9699029685b1ada4b1c0cb425462529f29b55123
aabdi <andreas.abdi@outlook.com>
2021-09-07 20:15:36 -0700

    everything is andreas

--- src/index.js
+++ src/index.js
@@ -141,14 +141,23 @@ class Game extends React.Component {
   }
   
 
-class MainPage extends React.Component {
+class AndreasSymbol extends React.Component {
     render() {
         return (
+            <h1 className="andreas-symbol">ANDREAS &nbsp;</h1>
+        );
+    }
+}
+
+class MainPage extends React.Component {
+
+    render() {
+        const symbols = Array.from({length: 300}, (i) => (<AndreasSymbol />));
+
+        return (
             <div className="main-page">
-                <div classname="header">
-                    <h1> Andreas Abdi </h1>
-                </div>
-                <Game />
+                <AndreasSymbol />
+                {symbols}
             </div> 
         );
     }
